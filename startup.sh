3ab6a10b396dbb48b2c43ad616d448b25d586dbc
Petchesi-Iulian <iamnotacat12@gmail.com>
2018-09-27 14:28:17 +0300

    Make directories for startup.

--- startup.sh
+++ startup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 set -e
 
+mkdir -p /usr/local/tomcat/webapps/geonetwork/WEB-INF/
 cp /tmp/config-summary.xml /usr/local/tomcat/webapps/geonetwork/WEB-INF/config-summary.xml
+
+mkdir -p /usr/local/tomcat/webapps/geonetwork/catalog/views/default/templates/
 cp /tmp/recordView.html /usr/local/tomcat/webapps/geonetwork/catalog/views/default/templates/recordView.html
+
+mkdir -p /usr/local/tomcat/webapps/geonetwork/WEB-INF/
 cp /tmp/config-lucene.xml /usr/local/tomcat/webapps/geonetwork/WEB-INF/config-lucene.xml
+
+mkdir -p /usr/local/tomcat/webapps/geonetwork/catalog/locales/
 cp /tmp/en-core.json /usr/local/tomcat/webapps/geonetwork/catalog/locales/en-core.json
+
+mkdir -p /usr/local/tomcat/webapps/geonetwork/images/harvesting/
 cp /tmp/EEA.png  /usr/local/tomcat/webapps/geonetwork/images/harvesting/EEA.png
+
 mkdir -p /usr/local/tomcat/webapps/geonetwork/WEB-INF/data/config/schema_plugins/
 cp -r /tmp/msfd-ind /usr/local/tomcat/webapps/geonetwork/WEB-INF/data/config/schema_plugins/msfd-ind
 
 chown -R root:root /usr/local/tomcat/webapps/
 
 exec /entrypoint.sh "$@"
-
-# if [ "$1" = 'catalina.sh' ]; then
-#
-#         mkdir -p "$DATA_DIR"
-#
-#         #Set geonetwork data dir
-#         export CATALINA_OPTS="$CATALINA_OPTS -Dgeonetwork.dir=$DATA_DIR"
-# fi
-#
-# exec "$@"
