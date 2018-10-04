0052fe30bced3b39ea67973a65354e2d8bd0547a
Petchesi-Iulian <iamnotacat12@gmail.com>
2018-10-04 13:07:53 +0300

    Changed file locations.

--- startup.sh
+++ startup.sh
@@ -5,44 +5,47 @@ echo "Starting sleep"
 sleep 5
 echo "End sleep"
 
-file=./usr/local/tomcat/webapps/geonetwork/WEB-INF/config-summary.xml
-if [ ! -e "$file" ]; then
+file=/usr/local/tomcat/webapps/geonetwork/WEB-INF/config-summary.xml
+if [ ! -f "$file" ]; then
     mkdir -p /usr/local/tomcat/webapps/geonetwork/WEB-INF/
     cp /webapps/config-summary.xml /usr/local/tomcat/webapps/geonetwork/WEB-INF/config-summary.xml
 fi
+ls $file
 
-file=./usr/local/tomcat/webapps/geonetwork/catalog/views/default/templates/recordView.html
-if [ ! -e "$file" ]; then
+file=/usr/local/tomcat/webapps/geonetwork/catalog/views/default/templates/recordView.html
+if [ ! -f "$file" ]; then
     mkdir -p /usr/local/tomcat/webapps/geonetwork/catalog/views/default/templates/
     cp /webapps/recordView.html /usr/local/tomcat/webapps/geonetwork/catalog/views/default/templates/recordView.html
 fi
+ls $file
 
-file=./usr/local/tomcat/webapps/geonetwork/WEB-INF/config-lucene.xml
-if [ ! -e "$file" ]; then
+file=/usr/local/tomcat/webapps/geonetwork/WEB-INF/config-lucene.xml
+if [ ! -f "$file" ]; then
     mkdir -p /usr/local/tomcat/webapps/geonetwork/WEB-INF/
     cp /webapps/config-lucene.xml /usr/local/tomcat/webapps/geonetwork/WEB-INF/config-lucene.xml
 fi
+ls $file
 
-file=./usr/local/tomcat/webapps/geonetwork/catalog/locales/en-core.json
-if [ ! -e "$file" ]; then
+file=/usr/local/tomcat/webapps/geonetwork/catalog/locales/en-core.json
+if [ ! -f "$file" ]; then
     mkdir -p /usr/local/tomcat/webapps/geonetwork/catalog/locales/
     cp /webapps/en-core.json /usr/local/tomcat/webapps/geonetwork/catalog/locales/en-core.json
 fi
+ls $file
 
-file=./usr/local/tomcat/webapps/geonetwork/images/harvesting/EEA.png
-if [ ! -e "$file" ]; then
+file=/usr/local/tomcat/webapps/geonetwork/images/harvesting/EEA.png
+if [ ! -f "$file" ]; then
     mkdir -p /usr/local/tomcat/webapps/geonetwork/images/harvesting/
     cp /webapps/EEA.png  /usr/local/tomcat/webapps/geonetwork/images/harvesting/EEA.png
-fi
+ fi
+ls $file
 
 echo "Finished script"
 
 if [ "$1" = 'catalina.sh' ]; then
-        echo "Mkdir data"
-        mkdir -p "$DATA_DIR"
-        echo "Intrat in IF"
-        #Set geonetwork data dir
-        export CATALINA_OPTS="$CATALINA_OPTS -Dgeonetwork.dir=$DATA_DIR"
-fi
-
+    echo "Mkdir data"
+    mkdir -p "$DATA_DIR"
+    echo "Intrat in IF"
+    #Set geonetwork data dir
+    export CATALINA_OPTS="$CATALINA_OPTS -Dgeonetwork.dir=$DATA_DIR"
 exec "$@"
